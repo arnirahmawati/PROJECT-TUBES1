77fa64e659f8d927351d846b5a258d3c57d03c2d
arnirahmawati <arnirahmat92@gmail.com>
2014-05-05 06:28:45 +0700

    update

--- index.php
+++ index.php
@@ -1,71 +1,73 @@
-<?php
-$sUrl = 'https://id.berita.yahoo.com/teknologi/';
-$sUrlSrc = getWebsiteContent($sUrl);
-
-// Load the source
-$dom = new DOMDocument();
-@$dom->loadHTML($sUrlSrc);
-
-$xpath = new DomXPath($dom);
-
-// step 1 - links:
-$aLinks = array();
-$vRes = $xpath->query(".//*[@id='top-stories']/div/h2/a[1]");
-foreach ($vRes as $obj) {
-    $aLinks[] = $obj->getAttribute('href');
+<html>
+<head>
+<title>Web Service</title>
+<style type="text/css">
+.sadaya{
+	color:#FFF;
+	width:1000px;
+	margin:auto
 }
-
-// step 2 - titles:
-$aTitles = array();
-$vRes = $xpath->query(".//*[@id='top-stories']/div/h2/a[1]/span");
-foreach ($vRes as $obj) {
-    $aTitles[] = $obj->nodeValue;
+.header{
+	margin:auto;
+	text-align:center
 }
-
-// step 3 - descriptions:
-$aDescriptions = array();
-$vRes = $xpath->query(".//*[@id='top-stories']/div/div[@class='body']/div[1]");
-foreach ($vRes as $obj) {
-    $aDescriptions[] = $obj->nodeValue;
+.anggota{
+	padding:6px;
+	float:left;
+	width:200px;
+	color:#C71585;
 }
-
-echo '<link href="css/styles.css" type="text/css" rel="stylesheet"/><div class="main">';
-echo '<h1>Using xpath for dom html</h1>';
-
-$entries = $xpath->evaluate('.//*[@id="headline-wrapper"]/div[1]/div/h2/span');
-echo '<h1>' . $entries->item(0)->nodeValue . 'yahoo news</h1>';
-
-$i = 0;
-foreach ($aLinks as $sLink) {
-    echo <<<EOF
-<div class="unit">
-    <a href="{$sLink}">{$aTitles[$i]}</a>
-    <div>{$aDescriptions[$i]}</div>
+.art{
+	padding:4px;
+	width:800px;
+	float:left;
+	color:#C71585;
+	height:auto
+}
+</style>
+</head>
+<body background="pict/pink.jpg">
+<div class="header">
+<center><img src="pict/gambar.jpg"></div>
+<div class="anggota"><font size=4 face="Comic Sans MS">
+<center>
+<b>created by: <br /> </b>
+</center>
+<ul>
+<li>Arni Rahmawati<br />
+117006054<br />
+<img src="pict/arni.jpg" width=100px/></li><br />
+<li>Fitria Nurwidyawati<br />
+117006067<br />
+<img src="pict/fitria.jpg" width=100px/></li><br />
+<li>Elis<br />
+117006082<br />
+<img src="pict/elis.jpg" width=100px/></li><br />
+</ul>
 </div>
-EOF;
-    $i++;
+<div class="art">
+<center><h1>Hello, welcome to our website...</h1></center><br />
+<br />
+<?php
+// we can specify a localy version or a remote xml as in example
+$xml = simplexml_load_file("http://www.antara.co.id/rss/news.xml");
+
+ 
+// now make a XPath selection: select all <item> tag.
+$items = $xml->xpath("/rss/channel/item");
+ 
+// Now, $items contains a class version of a <item> tag.
+// So, if I'd like to print only the title of news item:
+foreach($items as $item){
+//membuat variabel dari judul dan deskripsi (bisa juga menggunakan gambar)
+$title = $item->title;
+$description = $item->description;
+$link= $item->link;
+$tgl=date('l, d-m-Y');
+echo $tgl;
+//menampilkan deskripsi dan judul pada browser Anda
+echo '<p><b>'.$title.'</b><br />'.$description.'<br/>'.$link.'</p>';
 }
-echo '</div>';
-
-// this function will return page content using caches (we will load original sources not more than once per hour)
-function getWebsiteContent($sUrl) {
-    // our folder with cache files
-    $sCacheFolder = 'cache/';
-
-    // cache filename
-    $sFilename = date('YmdH').'.html';
-
-    if (! file_exists($sCacheFolder.$sFilename)) {
-        $ch = curl_init($sUrl);
-        $fp = fopen($sCacheFolder.$sFilename, 'w');
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, Array('User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15'));
-        curl_exec($ch);
-        curl_close($ch);
-        fclose($fp);
-    }
-    return file_get_contents($sCacheFolder.$sFilename);
-}
-
 ?>
+</body>
+</html>
